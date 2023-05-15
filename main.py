c17b8944ed0db3f7855543d824eb94a17c42066c
Tharin Tantayothin <tharinT@arv.co.th>
2023-05-15 22:15:12 +0700

    get porduct list last

--- main.py
+++ main.py
@@ -14,12 +14,60 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
 
-def check_exists_by_xpath(xpath):
-    try:
-        webdriver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
+def thai_number_to_int(value):
+    if value != "":
+        thai_numbers = {
+            'ศูนย์': 0,
+            'หนึ่ง': 1,
+            'สอง': 2,
+            'สาม': 3,
+            'สี่': 4,
+            'ห้า': 5,
+            'หก': 6,
+            'เจ็ด': 7,
+            'แปด': 8,
+            'เก้า': 9,
+            'สิบ': 10,
+            'ร้อย': 100,
+            'พัน': 1000,
+            'หมื่น': 10000,
+            'แสน': 100000,
+            'ล้าน': 1000000
+        }
+        int_value = 0
+        # Split the input value into its components
+        if value.find(".") > 0:
+            intNum, point = value.split(".")
+            value = "."+point
+            if "พัน" in value:
+                int_value = int(
+                    intNum)*thai_numbers["พัน"] + float(value.replace("พัน", ""))*thai_numbers["พัน"]
+            elif "หมื่น" in value:
+                int_value = int(intNum)*thai_numbers["หมื่น"] + float(
+                    value.replace("หมื่น", ""))*thai_numbers["หมื่น"]
+            elif "แสน" in value:
+                int_value = int(
+                    intNum)*thai_numbers["แสน"] + float(value.replace("แสน", ""))*thai_numbers["แสน"]
+            elif "ล้าน" in value:
+                int_value = int(
+                    intNum)*thai_numbers["ล้าน"] + float(value.replace("ล้าน", ""))*thai_numbers["ล้าน"]
+
+        else:
+            if "พัน" in value:
+                int_value = int(value.replace("พัน", ""))*thai_numbers["พัน"]
+            elif "หมื่น" in value:
+                int_value = int(value.replace("หมื่น", "")) * \
+                    thai_numbers["หมื่น"]
+            elif "แสน" in value:
+                int_value = int(value.replace("แสน", ""))*thai_numbers["แสน"]
+            elif "ล้าน" in value:
+                int_value = int(value.replace("ล้าน", ""))*thai_numbers["ล้าน"]
+            else:
+                int_value = value
+        return int_value
+    else:
+        return 0
+
 
 # driver = webdriver.Chrome(
 #     executable_path=r'D:\Work Dev\makewebbkk\scraping shoppee\chromedriver_win32\chromedriver')
@@ -64,59 +112,77 @@ product_name_list = []
 product_price_list = []
 product_sale_list = []
 
-driverTemp = driver
 driver.execute_script("document.body.style.MozTransform='scale(0.1)';")
 driver.execute_script('document.body.style.MozTransformOrigin = "0 0";')
-driver.execute_script("document.body.style.zoom='50%'")
+driver.execute_script("document.body.style.zoom='10%'")
 data = driver.page_source  # ดึงข้อมูลจากหน้าเว็บ
 soup = bs4.BeautifulSoup(data)  # จัดในรูปแบบ BeautifulSoup
-
+time.sleep(10)
 total_pages_element = int(soup.find(
     'span', {'class': 'shopee-mini-page-controller__total'}).text)
 print(total_pages_element)
+# all_product = soup.find_all('div', {'class': 'Mz89A3 WZO+p+ uAxOVF'})
+# all_product_price = soup.find_all('div', {'class': "KF1Uvz _3QBW9H"})
+# all_product_sale = soup.find_all('div', {'class': 'rOgDNT lNPX0P'})
+# print(len(all_product))
+# print(len(all_product_price))
+# print(len(all_product_sale))
+# all_product = soup.find_all('div', {'class': 'VptMHK Odl6HA GO6iCi'})
+# print(len(all_product))
 
-all_product = soup.find_all('div', {'class': 'VptMHK Odl6HA GO6iCi'})
-print(len(all_product))
+# for product in all_product:
+#     # print(product.text)
+#     product_name_list.append(product.text)
 
-for product in all_product:
-    # print(product.text)
-    product_name_list.append(product.text)
+# all_product_price = soup.find_all('div', {'class': "uwSW-2 qljqDx"})
 
-all_product_price = soup.find_all('div', {'class': "uwSW-2 qljqDx"})
-
-for product in all_product_price:
-    # print(product.text)
-    product_price_list.append(product.text)
+# for product in all_product_price:
+#     # print(product.text)
+#     product_price_list.append(product.text)
 
 
-all_product_sale = soup.find_all('div', {'class': "ZjwhVB YpOBv3"})
+# all_product_sale = soup.find_all('div', {'class': "ZjwhVB YpOBv3"})
 
-for product in all_product_sale:
-    # print(product.text)
-    product_sale_list.append(product.text)
+# for product in all_product_sale:
+#     # print(product.text)
+#     product_sale_list.append(product.text)
 
 # print(product_detail_list)
+# driver.close()
 
-# /html/body/div[1]/div/div[2]/div/div/div/div[2]/div/div[3]/div[2]/div[2]/div/div[3]/button[7]
-
-
-check_btn_next = 1
+check_btn_next = 0
 while check_btn_next < total_pages_element:
+    # options = webdriver.ChromeOptions()
+    # options.add_experimental_option("detach", True)
+    # driver = webdriver.Chrome(
+    #     options=options, executable_path="path/to/executable")
     page_url = f'https://shopee.co.th/nppbox?page={check_btn_next}'
+    print(f'https://shopee.co.th/nppbox?page={check_btn_next}')
     driver.get(page_url)
-    time.sleep(5)
+    time.sleep(3)
+    try:
+        myElem = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.ID, 'main')))
+        print("Page is ready!")
+    except TimeoutException:
+        print("Loading took too much time!")
+
     driver.execute_script("document.body.style.MozTransform='scale(0.1)';")
     driver.execute_script('document.body.style.MozTransformOrigin = "0 0";')
-    driver.execute_script("document.body.style.zoom='50%'")
+    driver.execute_script("document.body.style.zoom='5%'")
+    time.sleep(3)
     data = driver.page_source  # ดึงข้อมูลจากหน้าเว็บ
     soup = bs4.BeautifulSoup(data)  # จัดในรูปแบบ BeautifulSoup
 
-    all_product = soup.find_all('div', {'class': 'VptMHK Odl6HA GO6iCi'})
-    all_product_price = soup.find_all('div', {'class': "uwSW-2 qljqDx"})
-    all_product_sale = soup.find_all('div', {'class': "ZjwhVB YpOBv3"})
+    all_product = soup.find_all('div', {'class': 'Mz89A3 WZO+p+ uAxOVF'})
+    all_product_price = soup.find_all('div', {'class': "KF1Uvz _3QBW9H"})
+    all_product_sale = soup.find_all('div', {'class': 'rOgDNT'})
 
-    print(all_product_sale)
+    # print(all_product_sale)
+    print(len(all_product))
+    print(len(all_product_price))
     print(len(all_product_sale))
+
     for x in range(len(all_product)):
         if x > 5:
             product_name_list.append(all_product[x].text)
@@ -129,9 +195,12 @@ while check_btn_next < total_pages_element:
             product_sale_list.append(all_product_sale[x].text)
 
     check_btn_next += 1
-    time.sleep(2)
 
+driver.close()
 
+print(len(product_name_list))
+print(len(product_price_list))
+print(len(product_sale_list))
 header = ['name', 'price', 'sale']
 data_csv = []
 
@@ -145,12 +214,13 @@ with open('shopee.csv', 'w', encoding='UTF8') as f:
     for i in range(len(product_name_list)):
         data_csv = []
         data_csv.append(product_name_list[i])
-        data_csv.append(product_price_list[i])
-        data_csv.append(product_sale_list[i])
+        data_csv.append(product_price_list[i].replace("฿", ""))
+        data_csv.append(thai_number_to_int((product_sale_list[i].replace(
+            "ขายแล้ว ", "").replace(" ชิ้น", ""))))
         writer.writerow(data_csv)
 
 
-# with open('shopee.txt', 'w', encoding="utf-8") as f:
+# with open('shopee1.txt', 'w', encoding="utf-8") as f:
 #     for i in range(len(product_name_list)):
 #         f.write(product_name_list[i]+"," +
 #                 product_price_list[i]+","+product_sale_list[i])
