f439671c2df6ae24237b50452d4a34aa1e537249
Tharin Tantayothin <tharinT@arv.co.th>
2023-05-15 23:43:10 +0700

    product-linux

--- product-linux.py
+++ product-linux.py
@@ -10,62 +10,72 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
 import bs4
+
 # import pandas as pd
 import csv
 import time
+import io
 
-from flask import Flask, request
+from flask import Flask, request, send_file, make_response, Response
+
 app = Flask(__name__)
 
 
 def thai_number_to_int(value):
     if value != "":
         thai_numbers = {
-            'ศูนย์': 0,
-            'หนึ่ง': 1,
-            'สอง': 2,
-            'สาม': 3,
-            'สี่': 4,
-            'ห้า': 5,
-            'หก': 6,
-            'เจ็ด': 7,
-            'แปด': 8,
-            'เก้า': 9,
-            'สิบ': 10,
-            'ร้อย': 100,
-            'พัน': 1000,
-            'หมื่น': 10000,
-            'แสน': 100000,
-            'ล้าน': 1000000
+            "ศูนย์": 0,
+            "หนึ่ง": 1,
+            "สอง": 2,
+            "สาม": 3,
+            "สี่": 4,
+            "ห้า": 5,
+            "หก": 6,
+            "เจ็ด": 7,
+            "แปด": 8,
+            "เก้า": 9,
+            "สิบ": 10,
+            "ร้อย": 100,
+            "พัน": 1000,
+            "หมื่น": 10000,
+            "แสน": 100000,
+            "ล้าน": 1000000,
         }
         int_value = 0
         # Split the input value into its components
         if value.find(".") > 0:
             intNum, point = value.split(".")
-            value = "."+point
+            value = "." + point
             if "พัน" in value:
-                int_value = int(
-                    intNum)*thai_numbers["พัน"] + float(value.replace("พัน", ""))*thai_numbers["พัน"]
+                int_value = (
+                    int(intNum) * thai_numbers["พัน"]
+                    + float(value.replace("พัน", "")) * thai_numbers["พัน"]
+                )
             elif "หมื่น" in value:
-                int_value = int(intNum)*thai_numbers["หมื่น"] + float(
-                    value.replace("หมื่น", ""))*thai_numbers["หมื่น"]
+                int_value = (
+                    int(intNum) * thai_numbers["หมื่น"]
+                    + float(value.replace("หมื่น", "")) * thai_numbers["หมื่น"]
+                )
             elif "แสน" in value:
-                int_value = int(
-                    intNum)*thai_numbers["แสน"] + float(value.replace("แสน", ""))*thai_numbers["แสน"]
+                int_value = (
+                    int(intNum) * thai_numbers["แสน"]
+                    + float(value.replace("แสน", "")) * thai_numbers["แสน"]
+                )
             elif "ล้าน" in value:
-                int_value = int(
-                    intNum)*thai_numbers["ล้าน"] + float(value.replace("ล้าน", ""))*thai_numbers["ล้าน"]
+                int_value = (
+                    int(intNum) * thai_numbers["ล้าน"]
+                    + float(value.replace("ล้าน", "")) * thai_numbers["ล้าน"]
+                )
 
         else:
             if "พัน" in value:
-                int_value = int(value.replace("พัน", ""))*thai_numbers["พัน"]
+                int_value = int(value.replace("พัน", "")) * thai_numbers["พัน"]
             elif "หมื่น" in value:
-                int_value = int(value.replace("หมื่น", "")) * \
-                    thai_numbers["หมื่น"]
+                int_value = int(value.replace("หมื่น", "")) * thai_numbers["หมื่น"]
             elif "แสน" in value:
-                int_value = int(value.replace("แสน", ""))*thai_numbers["แสน"]
+                int_value = int(value.replace("แสน", "")) * thai_numbers["แสน"]
             elif "ล้าน" in value:
-                int_value = int(value.replace("ล้าน", ""))*thai_numbers["ล้าน"]
+                int_value = int(value.replace("ล้าน", "")) * thai_numbers["ล้าน"]
             else:
                 int_value = value
         return int_value
@@ -76,27 +86,31 @@ def thai_number_to_int(value):
 # driver = webdriver.Chrome(
 #     executable_path=r'D:\Work Dev\makewebbkk\scraping shoppee\chromedriver_win32\chromedriver')
 
-@app.route("/")
+
+@app.route("/product", methods=["GET"])
 def getData():
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), options=options)
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=options
+    )
 
-    driver.get('https://shopee.co.th/nppbox')
+    driver.get("https://shopee.co.th/nppbox")
 
     delay = 3  # seconds
     try:
         myElem = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.ID, 'main')))
+            EC.presence_of_element_located((By.ID, "main"))
+        )
         print("Page is ready!")
     except TimeoutException:
         print("Loading took too much time!")
     # เลือกภาษาไทย
     thai_button = driver.find_element(
-        By.XPATH, "/html/body/div[2]/div[1]/div[1]/div/div[3]/div[1]/button")
+        By.XPATH, "/html/body/div[2]/div[1]/div[1]/div/div[3]/div[1]/button"
+    )
 
     thai_button.click()
 
@@ -110,8 +124,9 @@ def getData():
     data = driver.page_source  # ดึงข้อมูลจากหน้าเว็บ
     soup = bs4.BeautifulSoup(data)  # จัดในรูปแบบ BeautifulSoup
     time.sleep(8)
-    total_pages_element = int(soup.find(
-        'span', {'class': 'shopee-mini-page-controller__total'}).text)
+    total_pages_element = int(
+        soup.find("span", {"class": "shopee-mini-page-controller__total"}).text
+    )
     print(total_pages_element)
 
     check_btn_next = 0
@@ -120,28 +135,28 @@ def getData():
         # options.add_experimental_option("detach", True)
         # driver = webdriver.Chrome(
         #     options=options, executable_path="path/to/executable")
-        page_url = f'https://shopee.co.th/nppbox?page={check_btn_next}'
-        print(f'https://shopee.co.th/nppbox?page={check_btn_next}')
+        page_url = f"https://shopee.co.th/nppbox?page={check_btn_next}"
+        print(f"https://shopee.co.th/nppbox?page={check_btn_next}")
         driver.get(page_url)
         time.sleep(5)
         try:
             myElem = WebDriverWait(driver, delay).until(
-                EC.presence_of_element_located((By.ID, 'main')))
+                EC.presence_of_element_located((By.ID, "main"))
+            )
             print("Page is ready!")
         except TimeoutException:
             print("Loading took too much time!")
 
         driver.execute_script("document.body.style.MozTransform='scale(0.1)';")
-        driver.execute_script(
-            'document.body.style.MozTransformOrigin = "0 0";')
+        driver.execute_script('document.body.style.MozTransformOrigin = "0 0";')
         driver.execute_script("document.body.style.zoom='5%'")
         time.sleep(5)
         data = driver.page_source  # ดึงข้อมูลจากหน้าเว็บ
         soup = bs4.BeautifulSoup(data)  # จัดในรูปแบบ BeautifulSoup
 
-        all_product = soup.find_all('div', {'class': 'Mz89A3 WZO+p+ uAxOVF'})
-        all_product_price = soup.find_all('div', {'class': "KF1Uvz _3QBW9H"})
-        all_product_sale = soup.find_all('div', {'class': 'rOgDNT'})
+        all_product = soup.find_all("div", {"class": "Mz89A3 WZO+p+ uAxOVF"})
+        all_product_price = soup.find_all("div", {"class": "KF1Uvz _3QBW9H"})
+        all_product_sale = soup.find_all("div", {"class": "rOgDNT"})
 
         # print(all_product_sale)
         print(len(all_product))
@@ -166,10 +181,10 @@ def getData():
     print(len(product_name_list))
     print(len(product_price_list))
     print(len(product_sale_list))
-    header = ['name', 'price', 'sale']
+    header = ["name", "price", "sale"]
     data_csv = []
 
-    with open('shopee.csv', 'w', encoding='UTF8') as f:
+    with open("shopee.csv", "w", encoding="UTF8") as f:
         writer = csv.writer(f)
 
         # write the header
@@ -180,6 +195,40 @@ def getData():
             data_csv = []
             data_csv.append(product_name_list[i])
             data_csv.append(product_price_list[i].replace("฿", ""))
-            data_csv.append(thai_number_to_int((product_sale_list[i].replace(
-                "ขายแล้ว ", "").replace(" ชิ้น", ""))))
+            data_csv.append(
+                thai_number_to_int(
+                    (product_sale_list[i].replace("ขายแล้ว ", "").replace(" ชิ้น", ""))
+                )
+            )
             writer.writerow(data_csv)
+
+    # return send_file('shopee.csv',mimetype='text/csv',as_attachment=True, ="data.csv")
+    output = io.StringIO()
+    writer2 = csv.writer(output)
+    line = ["name,price,sale"]
+    writer2.writerow(line)
+
+    for i in range(len(product_name_list)):
+        line = [
+            product_name_list[i]
+            + ","
+            + product_price_list[i].replace("฿", "")
+            + ","
+            + str(
+                thai_number_to_int(
+                    (product_sale_list[i].replace("ขายแล้ว ", "").replace(" ชิ้น", ""))
+                )
+            )
+        ]
+        writer2.writerow(line)
+
+    # return Response(
+    #     output,
+    #     mimetype="text/csv",
+    #     headers={"Content-Disposition": "attachment;filename=report.csv"},
+    # )
+    return send_file("shopee.csv", as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run()
